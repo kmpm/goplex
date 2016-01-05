b641f3daa33e71f2729ec54dc3ca02b17c8fe38b
kmpm <peter@birchroad.net>
2016-01-05 16:55:40 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+goplex
+======
+
+Trying to do something like kplex (NMEA multiplexer)
+but implemented in go.
+
---
+++ goplex.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	//"fmt"
+	//"github.com/tarm/serial"
+	"log"
+)
+
+func main() {
+	log.Printf("go plex again s\n")
+}
